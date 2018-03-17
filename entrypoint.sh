bc087517761598f8564568fd193e34d699058b77
Maik Hilker <maikhilker89@gmail.com>
2018-03-17 17:25:59 +0100

    Update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
 set -e
 
-CERT=/etc/murmur/certs/fullchain.pem
-KEY=/etc/murmur/certs/privkey.pem
+CERT="/etc/murmur/certs/$TLS_CERT_FILE"
+KEY="/etc/murmur/certs/$TLS_KEY_FILE"
+
+echo "Waiting for $CERT..."
+echo "Waiting for $KEY..."
 
 while [ ! -f "$CERT" ] && [ ! -f "$KEY" ]
 do
     inotifywait -q -t 60 -e create -e moved_to "$(dirname $CERT)"
     inotifywait -q -t 60 -e create -e moved_to "$(dirname $KEY)"
 done
+
+ln -s "/etc/murmur/certs/fullchain.pem" "$CERT"
+ln -s "/etc/murmur/certs/privkey.pem" "$KEY"
 
 echo "EXEC"
 exec "$@"
